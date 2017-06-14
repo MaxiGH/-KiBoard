fd1562138f45a05919eeaa5ea300257a12f87295
MaxiGH <maxigertz@googlemail.com>
2017-06-14 19:15:32 +0200

    initiated tracker branch, created TrackerInterface and Tracker3D

---
+++ KiBoard/tracker/Tracker3D.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KiBoard.tracker
+{
+    class Tracker3D : Tracker
+    {
+        public float getCoordinates()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
