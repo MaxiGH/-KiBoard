2a146dda85359a71427706c0c2b9217bd39b70c7
MaxiGH <maxigertz@googlemail.com>
2017-06-22 19:08:08 +0200

    first commit on calibration branch, implemented InitialCalibrator with hardcoded coordinates

--- KiBoard/Program.cs
+++ KiBoard/Program.cs
@@ -12,13 +12,18 @@ namespace KiBoard
 
         static void Main(string[] args)
         {
-            bool isRunning = true;
+            //calibrator testing
+            InitialCalibrator iniCal = new InitialCalibrator();
+            Console.WriteLine(iniCal.hasCalibrationPoints());
+
+            //tracker testing
+            /*bool isRunning = true;
             setupKinect();
             Tracker3D tracker = new Tracker3D(sensor, multiReader);
             while (isRunning) {
                 Vector3 trackedData = tracker.Coordinates;
                 Thread.Sleep(200);
-            }
+            }*/
         }
 
         private static void setupKinect()
---
+++ KiBoard/calibration/InitialCalibrator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace KiBoard
+{
+    class InitialCalibrator : Calibrator
+    {
+        private List<CalibrationPoint> list;
+
+
+        public InitialCalibrator()
+        {
+            CalibrationPoint calPoint1 = new CalibrationPoint(new Vector3(1, 0, 1), new Vector3(1, 0, 0));
+            CalibrationPoint calPoint2 = new CalibrationPoint(new Vector3(1, 1, 1), new Vector3(1, 1, 0));
+            CalibrationPoint calPoint3 = new CalibrationPoint(new Vector3(0, 1, 1), new Vector3(0, 1, 0));
+            CalibrationPoint calPoint4 = new CalibrationPoint(new Vector3(0, 0, 1), new Vector3(0, 0, 0));
+            list = new List<CalibrationPoint>();
+            list.Add(calPoint1);
+            list.Add(calPoint2);
+            list.Add(calPoint3);
+            list.Add(calPoint4);
+        }
+
+        public List<CalibrationPoint> getCalibrationPoints()
+        {
+            return list;
+        }
+
+        public bool hasCalibrationPoints()
+        {
+            int counter = 0;
+            foreach (var item in list)
+            {
+                counter++;
+            }
+
+            if (counter == 4)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        
+        //used later to get synchronized joint data per frame
+        public void tick()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
