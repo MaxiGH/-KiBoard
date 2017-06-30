95ad8de95eecc0b65284e15bc2a8230ce65327f1
Bluemi <bruno.schilling@protonmail.ch>
2017-06-30 13:29:17 +0200

    ~KeyCalibrator

--- KiBoard/calibration/KeyCalibrator.cs
+++ KiBoard/calibration/KeyCalibrator.cs
@@ -1,94 +1,68 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using KiBoard;
 using System.Numerics;
 
 namespace KiBoard.calibration
 {
     class KeyCalibrator : Calibrator
     {
-
-        private List<CalibrationPoint> list;
-        private CalibrationPoint calPoint1;
-        private CalibrationPoint calPoint2;
-        private CalibrationPoint calPoint3;
+        private List<CalibrationPoint> points;
+        private List<Vector3> wallPoints;
         private Tracker tracker;
-        private int state;
 
-        public KeyCalibrator(Tracker tracker) 
+        public KeyCalibrator(Tracker t) 
         {
-            this.list = new List<CalibrationPoint>();
-            this.tracker = tracker;
-            this.state = 0;
+            points = new List<CalibrationPoint>();
+            wallPoints = new List<Vector3>();
+            tracker = t;
+        }
+
+        private void initiateWallPoints()
+        {
+            wallPoints.Add(new Vector3(0, 0, 0));
+            wallPoints.Add(new Vector3(0, 1, 0));
+            wallPoints.Add(new Vector3(1, 0, 0));
         }
 
         public List<CalibrationPoint> getCalibrationPoints()
         {
-            return list;
-        }
-
-        public bool keyPressed()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void calibratePointOne()
-        {
-            Vector3 kinectVec = tracker.Coordinates;
-
-            calPoint1 = new CalibrationPoint(new Vector3(kinectVec.X, kinectVec.Y, kinectVec.Z), new Vector3(0, 0, 0));
-            list.Add(calPoint1);
-        }
-
-        public void calibratePointTwo()
-        {
-            Vector3 kinectVec = tracker.Coordinates;
-
-            calPoint2 = new CalibrationPoint(new Vector3(kinectVec.X, kinectVec.Y, kinectVec.Z), new Vector3(0, 1, 0));
-            list.Add(calPoint2);
-        }
-
-        public void calibratePointThree()
-        {
-            Vector3 kinectVec = tracker.Coordinates;
-
-            calPoint3 = new CalibrationPoint(new Vector3(kinectVec.X, kinectVec.Y, kinectVec.Z), new Vector3(1, 0, 0));
-            list.Add(calPoint3);
-        }
-
-        public void processCalibration()
-        {
-            if(state == 0 && keyPressed())
+            if (hasCalibrationPoints())
             {
-                calibratePointOne();
-                state = 1; 
+                return points;
             }
-            else if (state == 1 && keyPressed())
+            throw new MethodAccessException("can't get CalibrationPoints; points.Count=" + points.Count);
+        }
+
+        public int getNumberOfPoints()
+        {
+            return wallPoints.Count;
+        }
+
+        private bool keyPressed()
+        {
+            if (Console.KeyAvailable)
             {
-                calibratePointTwo();
-                state = 2;
+                Console.ReadKey();
+                return true;
             }
-            else if (state == 2 && keyPressed())
+            return false;
+        }
+
+        private void addPoint()
+        {
+            points.Add(new CalibrationPoint(tracker.Coordinates, wallPoints[points.Count]));
+        }
+
+        public void tick()
+        {
+            if (!hasCalibrationPoints() && keyPressed())
             {
-                calibratePointThree();
-                state = 3;
+                addPoint();
             }
         }
 
         public bool hasCalibrationPoints()
         {
-            return (list.Count == 3);
-        }
-
-        public void tick()
-        {
-            while (state < 3)
-            {
-                processCalibration();
-            }
+            return (points.Count == getNumberOfPoints());
         }
     }
-}
