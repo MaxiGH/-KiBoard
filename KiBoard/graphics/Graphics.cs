397a249d6d2ce69667a3c9523e558b849cba8698
ichinii <sven.salt@yahoo.de>
2017-07-02 14:54:54 +0200

    + renderAll() renders all items; ~ render() renders new items

--- KiBoard/graphics/Graphics.cs
+++ KiBoard/graphics/Graphics.cs
@@ -13,6 +13,7 @@ namespace KiBoard.graphics
         private System.Drawing.Bitmap pixels;
         private System.Drawing.Graphics gfx;
         private Matrix3x3 transform;
+        private int renderPos;
 
         public Graphics(System.Drawing.Graphics gfx)
         {
@@ -25,7 +26,13 @@ namespace KiBoard.graphics
 
             Size = size;
 
-            push(new Segment(new Vector2(0, 0), new Vector2(1, 1), 10.0f));
+            Line l = new Line(3);
+            l.nextPoint(new Vector2(0, 0));
+            l.nextPoint(new Vector2(0.7f, 0.1f));
+            l.nextPoint(new Vector2(0.5f, 0.5f));
+            l.nextPoint(new Vector2(0.2f, 0.3f));
+            l.nextPoint(new Vector2(0.6f, 0.8f));
+            push(l);
         }
 
         public int Count
@@ -44,7 +51,6 @@ namespace KiBoard.graphics
             get { return size; }
             set {
                 size = value;
-                //pixels.SetResolution((int)size.X, (int)size.Y);
                 pixels = new System.Drawing.Bitmap((int)Size.X, (int)Size.Y);
 
                 System.Drawing.Drawing2D.Matrix transform = new System.Drawing.Drawing2D.Matrix();
@@ -52,7 +58,12 @@ namespace KiBoard.graphics
             }
         }
 
-        public void clear()
+        System.Drawing.Bitmap getBitmap()
+        {
+            return pixels;
+        }
+
+        public void clearDrawables()
         {
             items.Clear();
         }
@@ -82,17 +93,38 @@ namespace KiBoard.graphics
                 items.RemoveAt(index);
         }
 
+        public void clear()
+        {
+            System.Drawing.Graphics g = System.Drawing.Graphics.FromImage(pixels);
+            g.Clear(Color.Black);
+        }
+
         public void render()
         {
             System.Drawing.Graphics g = System.Drawing.Graphics.FromImage(pixels);
-            g.Clear(Color.White);
+            g.Clear(Color.Black);
 
-            Matrix3x3 mat = Matrix3x3.multiply(Matrix3x3.identity().scale(new Vector2(size.X, size.Y)), transform);
+            Matrix3x3 mat = Matrix3x3.multiply(Matrix3x3.identity().scale(size), transform);
             foreach (Drawable i in items)
             {
                 i.draw(g, mat);
             }
 
+            g.Dispose();
+            gfx.DrawImage(pixels, 0, 0);
+        }
+
+        public void render234()
+        {
+            System.Drawing.Graphics g = System.Drawing.Graphics.FromImage(pixels);
+
+            Matrix3x3 mat = Matrix3x3.multiply(Matrix3x3.identity().scale(size), transform);
+            for (int i = renderPos; i < items.Count; i++)
+            {
+                items[i].draw(g, mat);
+            }
+
+            renderPos = items.Count;
             g.Dispose();
             gfx.DrawImage(pixels, 0, 0);
         }
