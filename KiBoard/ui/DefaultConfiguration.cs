136b5d75e6185004e1671bcba4675def018a3584
Bluemi <bruno.schilling@protonmail.ch>
2017-07-02 18:05:45 +0200

    added UIManager added UIElement(s)

---
+++ KiBoard/ui/DefaultConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Numerics;
+
+namespace KiBoard.ui
+{
+    class DefaultConfiguration : UIConfiguration
+    {
+        public List<UIElement> createConfiguration()
+        {
+            List<UIElement> list = new List<UIElement>();
+            try {
+                list.Add(new Button(new Vector2(0.15f, 0.15f), new Vector2(0.1f, 0.1f), Bitmap.FromFile("resources/Pen.png")));
+            }
+            catch (System.IO.FileNotFoundException e)
+            {
+                Console.WriteLine("couldnt load \"resources/Pen.png\"");
+            }
+            return list;
+        }
+    }
+}
---
+++ KiBoard/ui/UIElement.cs
@@ -0,0 +1,12 @@
+﻿using System.Drawing;
+using System.Numerics;
+
+namespace KiBoard.ui
+{
+    interface UIElement
+    {
+        void render(Graphics gfx, Size WindowSize);
+        bool touches(Vector2 vec);
+        void setVisible(bool visible);
+    }
+}
